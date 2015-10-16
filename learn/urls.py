9dae5af0c6de19636fb4d10e8a44b65dcaf02e34
frankwzhg <frankwzhg@hotmail.com>
2015-10-16 15:58:13 +0800

    video play

--- learn/urls.py
+++ learn/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
 from learn import views
-from registration.backends.simple.views import RegistrationView
+from django.views.generic import TemplateView
+
 
 # Create a new class that redirects the user to the index page, if successful at logging
 
 
 urlpatterns = patterns('',
-            url(r'^$', views.video, name='player'),
-            url(r'^videoupload', views.video_upload_view, name='video_upload'),
+            url(r'^videodata/$', views.video, name='video_data'),
+            url(r'^videoupload/$', views.video_upload_view, name='video_upload'),
+            url(r'^$', TemplateView.as_view(template_name='learn/player.html'), name='video_player'),
             )
--- learn/views.py
+++ learn/views.py
@@ -3,50 +3,20 @@ from tango.settings import *
 from learn.forms import *
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
+from django.views.generic import TemplateView
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core import serializers
 # Create your views here.
 from os import walk
+
 def video(request):
-    # videos = {}
     videos_file = VideosModel.objects.all()
     videos_file = serializers.serialize('json', videos_file)
-    # struct = json.loads(videos_file)
-    # # print struct[0]
-    # videos_file = videos_file[1:-1]
-    # # # print videos_file
-    # videos_file = json.dumps(videos_file)
-    # print type(videos_file)
-    # for video in videos_file:
-    #     videos['video_name'] = video.video_name
-    #     videos['video_path'] = video.video_path
-    # print videos
-    # videos_file = serializers.serialize('json', videos_file)
-    # videos_file = videos_file[1:-1]
-    # print videos_file
-    # for item in videos_file:
-    #     print item.video_path
-    # data = [{'video_name':item.video_name} for item in videos_file ]
-    # json_data = json.dumps(data)
-    # videos_file = serializers.serialize('json', videos_file)
-    # print type(videos_file)
-    # path = MEDIA_URL + "learn/"
-    # path = BASE_DIR + path
-    # files = []
-    # for (dirpath, dirname, filename) in walk(path):
-    #     files.extend(filename)
-    # videos["playlist1"] = files
-    # videos['media'] = MEDIA_URL
-    # video_json = json.load(videos_file)
-    # print type(video_json)
-    # return HttpResponse(videos_file, content_type='application/json')
-    test = json.dumps({'test': {'num':'12345', 'arr': {'abc':[1,2,3]}}})
-    # print test
-    # return render(request, "learn/player.html", videos)
-    return render_to_response('learn/player.html', {'video_json': videos_file, 'test':test})
+    return HttpResponse(videos_file, content_type='application/json')
 
 def video_upload_view(request):
+    arg = {}
     user = request.user
     if request.method == 'POST':
         print request.POST.get('video_name')
@@ -60,6 +30,7 @@ def video_upload_view(request):
                 form_count.video_path = request.FILES['video_path']
                 print form_count.video_path
             form_count.save()
-            return HttpResponse('your upload sccuess')
+            arg['sccuess'] = "your file uploaded sccuessfully!"
+            return HttpResponseRedirect('/learn/')
 
-    return render_to_response('learn/video_upload.html', {}, RequestContext(request))
+    # return render_to_response('learn/player.html', arg, RequestContext(request))
